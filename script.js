5e6b949ac972d2c892a8ddf927b4e43644ea1acb
kiyohiroleo <kiyohiroleo@gmail.com>
2025-01-28 11:13:10 -0300

    css js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const links = document.querySelectorAll('nav ul li a');
-    
-    links.forEach(link => {
+  const links = document.querySelectorAll('nav ul li a');
+  
+  links.forEach(link => {
+    // Verifica se o link é interno (com href começando com '#')
+    if (link.getAttribute('href').startsWith('#')) {
       link.addEventListener('click', (e) => {
         e.preventDefault();
         const targetId = link.getAttribute('href').substring(1);
         const targetSection = document.getElementById(targetId);
-  
+
         window.scrollTo({
           top: targetSection.offsetTop,
           behavior: 'smooth',
         });
       });
-    });
+    }
   });
+});
