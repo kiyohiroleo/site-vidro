eed0f78fc753f6c230e412a00b8c30bbb9394a31
kiyohiroleo <kiyohiroleo@gmail.com>
2025-01-30 10:50:49 -0300

    card feito falta as animaçoes

--- script.js
+++ script.js
@@ -9,11 +9,31 @@ document.addEventListener("DOMContentLoaded", () => {
         const targetId = link.getAttribute('href').substring(1);
         const targetSection = document.getElementById(targetId);
 
+        // Ajuste: subtrai a altura do cabeçalho
+        const headerHeight = document.querySelector('header').offsetHeight;
+
         window.scrollTo({
-          top: targetSection.offsetTop,
+          top: targetSection.offsetTop - headerHeight, // Subtrai a altura do cabeçalho
           behavior: 'smooth',
         });
       });
     }
   });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const inicioSection = document.getElementById('inicio');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        inicioSection.classList.add('visible');
+        observer.unobserve(inicioSection); // Para de observar após a animação ser aplicada
+      }
+    });
+  }, {
+    threshold: 0.1 // A animação será disparada quando 10% da seção estiver visível
+  });
+
+  observer.observe(inicioSection);
 });
